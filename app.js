5c1f0095eaff34e24c8d85af92161eef8abbe6e4
nikhilreddy11ie253 <72740560+nikhilreddy11ie253@users.noreply.github.com>
2021-07-11 15:34:15 +0530

    seven random numbers 2b

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const sentence='I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher';
-
-function varietyOfWords(sentence){
-    let set=new Set(sentence.split(' '));
-    return set.size;
-}
-console.log(varietyOfWords(sentence));
+let getRandomNumbers=function(start,range){
+        var arr = [];
+    while(arr.length < 7){
+        var i = Math.floor(Math.random() * 9);
+        if(arr.indexOf(i) === -1) arr.push(i);
+    }
+    console.log(arr);
+    
+    }
+    console.log(getRandomNumbers(0,9));
