03347609e0c39175becfc6ce3f65ae815a1a7d5b
nikhilreddy11ie253 <72740560+nikhilreddy11ie253@users.noreply.github.com>
2021-07-11 15:22:34 +0530

    1d

---
+++ app.js
@@ -0,0 +1,7 @@
+const sentence='I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher';
+
+function varietyOfWords(sentence){
+    let set=new Set(sentence.split(' '));
+    return set.size;
+}
+console.log(varietyOfWords(sentence));
