0aad64a7dcc01f06400eec1ef4ad0347de81c778
nikhilreddy11ie253 <72740560+nikhilreddy11ie253@users.noreply.github.com>
2021-07-11 22:20:54 +0530

    2c

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let n=7;
-let star ="";
-for(let i=0; i<=n;i++) {
-    for(let j=0; j<i; j++) {
-        star +="*";
+array1=["A","B","C","D"];
+function reverseArray(array1) {
+    var array2 = [];
+    while (array1.length) {
+      array2.push(array1.pop());
     }
-    star +="\n";
-}
-console.log(star);
+    return array2;
+  }
+  
+  console.log(reverseArray(array1));
